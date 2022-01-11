7ae42e981327db1819003cef937db114f2b36040
RY2291 <prgr2498@gmail.com>
2022-01-11 23:36:27 +0900

    [add]8.5

--- section/chapter_8.5.rb
+++ section/chapter_8.5.rb
@@ -133,3 +133,26 @@ p product.price_tag
   p user.name
   p user.change_name
   p user.name
+
+  # モジュールとインスタンス変数
+  # モジュール内で定義したメソッドの中でインスタンス変数を読み書きすると、include先のクラスの変数を読み書きしたことと同じなる。
+  module NameChanger
+    def change_name
+      self.name = "アリス"
+    end
+  end
+
+  class User
+    include NameChanger
+
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+  end
+
+  user = User.new("alice")
+  p user.name
+  p user.change_name
+  p user.name
